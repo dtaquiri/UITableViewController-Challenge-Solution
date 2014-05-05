613f2cb1bb68d3ea81370729c41fbf48594d3c51
dtaquiri <dtaquiri@hotmail.com>
2014-05-05 18:11:33 -0500

    Initial Commit

---
+++ UITableViewController Challenge Solution/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Diego Taquiri Diaz on 5/05/14.
+//  Copyright (c) 2014 iLDT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UITableViewController Challenge Solution/CCViewController.h
@@ -0,0 +1,14 @@
+//
+//  CCViewController.h
+//  UITableViewController Challenge Solution
+//
+
+//  Copyright (c) 2014 iLDT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
